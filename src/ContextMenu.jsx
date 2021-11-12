bb157d2fb3a8403ef0f9a499c6ce73a892651c5f
Johan Munneke <johanmunneke94@gmail.com>
2021-11-12 16:37:38 +0100

    First setup

--- src/ContextMenu.jsx
+++ src/ContextMenu.jsx
@@ -1,10 +1,36 @@
-import { Component, createElement } from "react";
+import { Component, createElement, Fragment } from "react";
 
-import { HelloWorldSample } from "./components/HelloWorldSample";
+//import { HelloWorldSample } from "./components/HelloWorldSample";
 import "./ui/ContextMenu.css";
 
 export default class ContextMenu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showContent: false
+        };
+    }
+
+    handleRightClick = e => {
+        e.preventDefault();
+        this.setState({ showContent: true });
+    };
+
+    handleClick = e => {
+        e.preventDefault();
+        this.setState({ showContent: false });
+    };
+
     render() {
-        return <HelloWorldSample sampleText={this.props.sampleText} />;
+        return (
+            <Fragment>
+                <div onClick={e => this.handleClick(e)} onContextMenu={e => this.handleRightClick(e)} on>
+                    {this.props.content}
+                </div>
+                {this.state.showContent ? (
+                    <div onClick={e => this.handleClick(e)}>this.props.clickContent</div>
+                ) : undefined}
+            </Fragment>
+        );
     }
 }
